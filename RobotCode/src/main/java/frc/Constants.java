855ff226381d3063e177ad26c588a0a3c24bc38a
spiresFrc9106 <ccrobotics@grcatholiccentral.org>
2023-04-10 17:16:08 -0400

    Builds and simulates
    Signed-off-by: spiresFrc9106 <ccrobotics@grcatholiccentral.org>

--- RobotCode/src/main/java/frc/Constants.java
+++ RobotCode/src/main/java/frc/Constants.java
@@ -15,6 +15,18 @@ import edu.wpi.first.math.util.Units;
 import frc.robot.Robot;
 
 public class Constants {
+    public static final Mode currentMode = Mode.REAL;
+
+    public static enum Mode {
+      /** Running on a real robot. */
+      REAL,
+  
+      /** Running a physics simulator. */
+      SIM,
+  
+      /** Replaying from a log file. */
+      REPLAY
+    }
     //////////////////////////////////////////////////////////////////
     // Drivetrain Physical
     //////////////////////////////////////////////////////////////////
